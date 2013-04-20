704d0e0957413bf164fbe28232ee2c42408d9398
lbaby <lsylny@gmail.com>
2013-04-20 15:07:52 +0200

    delete some file

--- shell/insert_excel.py
+++ shell/insert_excel.py
@@ -3,20 +3,66 @@
 #author : lbaby   lsylny@gmail.com
 ''' Insert an excel file into oracle db
     usage:
-    insert_excel.py db=usr/passwd@db excel=all_data.xlsx sql="insert into aa values(:n1, :n2, to_number(:n3), NULL)"
+    insert_excel.py db=usr/passwd@db excel=all_data.xlsx 
+	sql="insert into aa values(:n1, :n2, to_number(:n3), NULL)"
     Dependent package : xlrd, cx_Oracle
 '''
+
 import re
 import os
 import sys
 import xlrd
+import datetime
 import cx_Oracle
 
-os.environ['NLS_LANG'] = 'SIMPLIFIED CHINESE_CHINA.UTF8'
+clas parse_insert:
+	def __init__(self, sql):
+		pass
 
-re_bind_var = re.compile(r':\w+\W*?')
-re_word_strip=re.compile(r'\W+')
-node_prefix='n' #For node count, n1 means node 1
+	def get_table():
+		return self.table
+
+	def get_fields():
+		return self.fields
+
+
+
+#Parse the sql , inspect target table structure , do necessary format conversion
+def sql_executor(cursor, sql):
+	#For closure imitation
+	#Encapsule a sql statement in a class
+	class bind_sql_to_executor:
+		def __init__(self, cursor, sql)
+			'''
+			execute a sql with lines
+			insert into client_node 
+			values((select nvl(max(client_id),1) from client_node),:n4,:n5,
+			to_number(:n6),:n7,to_number(:n9),:n8,:n10, NULL,:n3)
+			'''
+			self.re_bind_var = re.compile(r':\w+\W*?')
+			self.re_word_strip=re.compile(r'\W+')
+			self.node_prefix='n' #For node count, n1 means node 1
+			self.sql = sql
+			#cx_Oracle require exact number of key in dictionary
+			#So I must extract the bindind variable from sql 
+			#bind_var_list (':va1',':va2')
+			#var_key_list('va1','va2')
+			bind_var_list=self.re_bind_var.findall(sql)
+			self.var_key_list=list()
+			for s in bind_var_list:
+				var_key = self.re_word_strip.sub('',s)
+				if var_key not in self.var_key_list:
+					self.var_key_list.append(var_key)
+			self.tar
+			#parse
+		def exec_using(self, data)
+			#Fill a dict for variable bind data => {'n1':'v1', 'n2':'v2'...}
+			val_dict=dict()
+			for key in self.var_key_list:
+				val_dict[key] = field[int(key.replace(self.node_prefix,'')) -1]
+			cursor.execute(self.sql, val_dict)	
+	#Return analysed sql executor 
+	return bind_sql_to_executor(cursor, sql).exec_using
 
 def read_excel(excel):
 	book = xlrd.open_workbook(excel)
@@ -24,33 +70,6 @@ def read_excel(excel):
 		sh = book.sheet_by_index(n)
 		for r in range(sh.nrows):
 			yield sh.row_values(r)
-
-def exec_sql(cursor,sql, oneline):
-	'''
-	execute a sql with lines
-	insert into client_node 
-	values((select nvl(max(client_id),1) from client_node),:n4,:n5,
-	to_number(:n6),:n7,to_number(:n9),:n8,:n10, NULL,:n3)
-
-	'''
-	#cx_Oracle require exact number of key in dictionary
-	#So I must extract the bind variable from sql so that combine the dictionary
-
-	#bind_var_list (':va1',':va2')
-	#var_key_list('va1','va2')
-	bind_var_list=re_bind_var.findall(sql)
-	var_key_list=list()
-	for s in bind_var_list:
-		var_key = re_word_strip.sub('',s)
-		if var_key not in var_key_list:
-			var_key_list.append(var_key)
-	field=oneline
-	#Field are filled into dict v1|v2|v3 ==> {'n1':'v1', 'n2':'v2'
-	#insert one line
-	val_dict=dict()
-	for key in var_key_list:
-		val_dict[key] = field[int(key.replace(node_prefix,'')) -1]
-	cursor.execute(sql, val_dict )
 
 def parse_arg(arg):
 	'''
@@ -67,16 +86,18 @@ if __name__ == '__main__':
 	if len(sys.argv) != 4:
 		raise RuntimeError('''
 		usage:
-    		insert_excel.py db=usr/passwd@db excel=all_data.xlsx sql="insert into aa values(:n1, :n2, to_number(:n3), NULL)"
+    		insert_excel.py db=usr/passwd@db excel=all_data.xlsx  \
+		sql="insert into aa values(:n1, :n2, to_number(:n3), NULL)"
     		''')
 	argv = parse_arg(sys.argv[1:])
 	db = cx_Oracle.connect(argv['DB'])
 	cursor = db.cursor()
+	sql_exec =  sql_executor(argv['SQL'])
 	for row in read_excel(argv['EXCEL']):
 		try:
-			exec_sql(cursor, argv['SQL'], row)
+			sql_exec(cursor, row)
 		except Exception, e:
 			print >>sys.stderr, row 
-			print >>sys.sterr, e
+			print >>sys.stderr, e
 	cursor.close()
 	db.commit()
